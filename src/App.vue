4e161c449fd14a0668797b533849d0c24bb5b938
Elaoud, Mohamed Amin <Mohamed-Amin.Elaoud@adesso.de>
2024-07-29 12:24:47 +0200

    first banner

--- src/App.vue
+++ src/App.vue
@@ -63,34 +63,52 @@ const updateKey = ref(0);
 <template>
   <Vueform :key="updateKey" v-bind="vueform" />
 
-
   <div id="widget-content" class="banner">
     <div class="image">
-      <img src="../src/assets/1kqp4d.jpg" alt="Girl in a jacket">
-
+      <img :src="formValues.link || '../src/assets/1kqp4d.jpg'" alt="Girl in a jacket">
     </div>
-    <h1> {{ formValues.headline }}</h1>
-    <div v-html="formValues.content"></div>
+    <div id="text-content">
+      <h3>{{ formValues.headline }}</h3>
+      <div v-html="formValues.content"></div>
+      <div class="button">
+        <button type="button">{{formValues.button}}</button>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <style scoped>
 .banner {
   display: flex;
-  align-items: flex-start; /* Align items to the start of the flex container */
+  text-align: left;
+  align-items: flex-start;
+  background-color: rgb(241, 236, 229);
+  /* Align items to the start of the flex container */
 }
 
 .image {
-  flex-shrink: 0; /* Prevent the image from shrinking */
-  margin-right: 20px; /* Space between the image and the text content */
+  flex-shrink: 0;
+  /* Prevent the image from shrinking */
+  margin-right: 20px;
+  /* Space between the image and the text content */
 }
 
 .image img {
-  width: 100px;
+  width: 215;
   height: 100px;
 }
 
 #text-content {
-  flex-grow: 1; /* Allow the text content to grow and take up the remaining space */
+  flex-grow: 1;
+  /* Allow the text content to grow and take up the remaining space */
+  display: flex;
+  flex-direction: column;
+  /* Stack the headline and content vertically */
+}
+
+h3 {
+  margin: 0;
+  /* Remove default margin from the headline */
 }
 </style>
